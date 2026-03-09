c76b8fc9278ad56ffbc516f984a23f0b7dcbe58b
Rob Beal <h6oay4sh@kohi.org.uk>
2026-03-09 01:29:51 +0000

    chore: use a new clientId

--- homeassistant/components/yoto/const.py
+++ homeassistant/components/yoto/const.py
@@ -7,4 +7,4 @@ DOMAIN = "yoto"
 DEFAULT_SCAN_INTERVAL = 5  # minutes
 SCAN_INTERVAL = timedelta(minutes=DEFAULT_SCAN_INTERVAL)
 
-CLIENT_ID = "KFLTf5PCpTh0yOuDuyQ5C3LEU9PSbult"
+CLIENT_ID = "eWrqzx1Eg527tu2DbezjgqLoczx6ekrA"
